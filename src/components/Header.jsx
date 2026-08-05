4f1346e10655e6129aa5d74d78a8c4c06223b0c9
Zeynab Jalalian <zjalalianweb@gmail.com>
2026-08-05 13:30:03 +0330

    header responsive

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import MenuItemLists from "./MenuItemLists";
+import { Menu, X } from "lucide-react";
 
 function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleMenu() {
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <header className="flex justify-around items-center px-12 py-5 bg-stone-50 border-b border-stone-200 shadow-[0_8px_20px_rgba(0,0,0,0.05)]">
+    <header className="relative flex justify-between items-center px-6 py-5 bg-stone-50 border-b border-stone-200 shadow-[0_8px_20px_rgba(0,0,0,0.05)] md:px-12">
       <Logo />
 
-      <nav>
+      {/* Desktop Menu */}
+      <nav className="hidden md:block">
         <MenuItemLists />
       </nav>
+
+      {/* Mobile Button */}
+      <button
+        onClick={handleMenu}
+        className="md:hidden p-2 rounded-xl cursor-pointer transition hover:scale-110"
+      >
+        {isOpen ? <X size={32} /> : <Menu size={32} />}
+      </button>
+
+      {/* Mobile Menu */}
+      {isOpen && (
+        <nav className="absolute top-full right-0 w-full bg-stone-50 shadow-lg py-6 md:hidden">
+          <MenuItemLists />
+        </nav>
+      )}
     </header>
   );
 }
--- src/components/MenuItemLists.jsx
+++ src/components/MenuItemLists.jsx
@@ -10,10 +10,13 @@ function MenuItemLists() {
   ];
   return (
     <>
-      <ul className="flex gap-8 text-xl">
+      <ul className="flex flex-col items-center gap-6 text-xl md:flex-row md:gap-8">
         {menuItems.map((item) => {
           return (
-            <li key={item}>
+            <li
+              key={item}
+              className="w-full text-center border-b border-stone-200 py-2.5 last:border-none md:w-auto md:border-none md:py-0"
+            >
               <a className="font-medium text-stone-700 hover:text-amber-700 transition-colors duration-300 cursor-pointer">
                 {item}
               </a>
