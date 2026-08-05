7646ccc66900bc714416beeb7ab583ee3517b8fd
Zeynab Jalalian <zjalalianweb@gmail.com>
2026-08-05 16:09:37 +0330

    contact

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import FeaturedBooks from "./components/FeaturedBooks";
 import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Hero />
       <FeaturedBooks />
       <About />
+      <Contact />
     </>
   );
 }
